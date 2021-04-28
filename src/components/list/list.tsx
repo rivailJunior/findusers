c6ac5b79ab7b5142dc39ccef7d717161924b122f
rivailtarkuss <rivail@tarkuss.com.br>
2021-04-28 17:06:13 -0400

    [ADD] - Sort repository list

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ErrorFeedback } from '../errorFeedback/errorFeedback';
 import styles from './list.module.scss';
 import moment from 'moment'
+import { GridContainer, GridContainerItem } from '../gridContainer/gridContainer';
+
 
 type iList = {
     data: object[],
@@ -9,25 +11,44 @@ type iList = {
 }
 
 const ListRepositories = ({ data, handleClick }: iList): JSX.Element => {
+    const [showReverse, setShowReverse] = useState(false);
+
+    const listStyle = showReverse ? `${ styles.item } ${ styles.listReverse }` : styles.item;
     return data.length ? (
-        <div data-testid="list" className={styles.item}>
-            {data.map((item: any, index) => {
-                return (
-                    <div key={index} className={styles.itemLi} onClick={() => handleClick(item)} data-testid="listItem">
-                        <div className={styles.left}>
-                            {item.stargazers_count}
-                        </div>
-                        <div className={styles.right}>
-                            <div className={styles.rightTitle}>
-                                {item?.name}
+        <div>
+            <div className={styles.listTitlesInformations}>
+                <div className={styles.stars}>
+                    <label htmlFor="">Estrelas</label>
+                </div>
+                <div className={styles.description}>
+                    <label htmlFor="">Description</label>
+                </div>
+                <div className={styles.button}>
+                    <button data-testid="btnReverse" className={styles.btnReverse} onClick={() => setShowReverse(!showReverse)}>Sort</button>
+                </div>
+            </div>
+
+
+            <div data-testid="list" className={listStyle}>
+
+                {data.map((item: any, index) => {
+                    return (
+                        <div key={index} className={styles.itemLi} onClick={() => handleClick(item)} data-testid="listItem">
+                            <div className={styles.left}>
+                                {item.stargazers_count}
                             </div>
-                            <div className={styles.rightSub}>
-                                <div>{moment(item.created_at).format('DD/MM/YYYY')}</div>
-                                <div> <a href={item.clone_url} target="_blank" rel="noopener noreferrer">{item.clone_url}</a></div>
+                            <div className={styles.right}>
+                                <div className={styles.rightTitle}>
+                                    {item?.name}
+                                </div>
+                                <div className={styles.rightSub}>
+                                    <div>{moment(item.created_at).format('DD/MM/YYYY')}</div>
+                                    <div> <a href={item.clone_url} target="_blank" rel="noopener noreferrer">{item.clone_url}</a></div>
+                                </div>
                             </div>
-                        </div>
-                    </div>)
-            })}
+                        </div>)
+                })}
+            </div>
         </div>
     ) : (
         <ErrorFeedback title="Nada encontrado" />
--- test/list.test.tsx
+++ test/list.test.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import { render, cleanup, screen, fireEvent } from '@testing-library/react'
+import { render, cleanup, screen, fireEvent, act } from '@testing-library/react'
 import renderer from 'react-test-renderer'
 import '@testing-library/jest-dom/extend-expect'
 import { ListRepositories } from '../src/components/list/list'
+import { repositoryMockValue } from '../src/model/repository'
 
-afterEach(cleanup)
+import { unmountComponentAtNode } from "react-dom";
 
-const arrayFake = Array(10).fill(1)
+let container = null;
+beforeEach(() => {
+    // configurar o elemento do DOM como o alvo da renderização
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    // limpar na saída
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+const arrayFake = Array(10).fill(repositoryMockValue)
 
 const fakeFn = jest.fn()
 describe('List Component', () => {
@@ -17,17 +32,29 @@ describe('List Component', () => {
     })
 
     test('Show correct total itens on list', () => {
-        const { getAllByTestId } = render(<ListRepositories data={arrayFake} handleClick={fakeFn} />)
+        const { getAllByTestId } = render(<ListRepositories data={arrayFake} handleClick={fakeFn} />, container)
         const listLi = getAllByTestId('listItem')
         expect(listLi).toHaveLength(arrayFake.length)
     })
 
     test('Click on list item', () => {
-        const { getAllByTestId } = render(<ListRepositories data={arrayFake} handleClick={fakeFn} />)
+        const { getAllByTestId } = render(<ListRepositories data={arrayFake} handleClick={fakeFn} />, container)
         const listLi = getAllByTestId('listItem')
         fireEvent.click(listLi[0])
         expect(fakeFn).toHaveBeenCalledTimes(1)
-        // expect(listLi).toHaveLength(arrayFake.length)
+    })
+
+
+    test('Click on sort', (done) => {
+        render(<ListRepositories data={arrayFake} handleClick={fakeFn} />, container)
+        const list = screen.getByTestId('list')
+        const btnReverse = screen.getByTestId("btnReverse");
+        expect(list.className).toBe("item");
+        fireEvent.click(btnReverse)
+        const sortedList = screen.getByTestId('list')
+        expect(sortedList.className).toBe("item listReverse")
+        done()
+
     })
 
 })
